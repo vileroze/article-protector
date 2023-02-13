97db44e4286b0c54ca27c68bc4e3c3ad7565c00d
vileroze <50352881+vileroze@users.noreply.github.com>
2023-02-13 13:42:58 +0545

    Created metafiled checkbox
    created an overlay while displaying article for users that have not logged in.
    
    Users can login but shows "headers already set " error, which is solved after page refresh
    
    checks if post is premium or not and then only is the form displayed

--- article-protector.php
+++ article-protector.php
@@ -29,23 +29,120 @@ function article_protector_deactivate() {
 register_deactivation_hook( __FILE__, 'article_protector_deactivate' );
 
 
-//show only 100 words of premium article
-add_filter( 'the_content', 'addToEnd' );
-function addToEnd( $content ){
-    $result = substr( $content, 0, 499 ) . "To continue reading please login";
 
-    $result .= 
-    '
-        <form action="" method="post">
-            <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
+// register style on initialization
+add_action('init', 'register_custom_style');
+function register_custom_style(){
+	wp_register_style( 'custom-plugin-style', plugins_url('/public/css/article-protector-public.css', __FILE__), false, '1.0.0', 'all');
+}
+
+// use the style above
+add_action('wp_enqueue_scripts', 'enqueue_style');
+function enqueue_style(){
+	wp_enqueue_style( 'custom-plugin-style' );
+}
+
+
+function article_protector_login_form(){
+
+    ob_start(); 
+    ?>
+
+        <div class="premium-post-div">
+            <h5>To continue reading this article please login</h5>
+            <form action="" method="post">
+                <label for="username"><b>Username</b></label>
+                <input type="text" placeholder="Enter Username" name="username" required>
         
-            <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+                <label for="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required>
 
-            <input type="submit" name="uLogin" value="Submit">
+                <input type="submit" name="userlogin" value="LOGIN">
+            </form>
+        </div>
+
+    <?php 
+
+    if( isset($_POST['userlogin']) ){
+        $user_name = $_POST['username'];
+        $user_pass = $_POST['psw'];
+
+        $creds = [
+            'user_login'    => $user_name,
+            'user_password' => $user_pass,
+            'remember'      => false
+        ];
+    
+        $user = wp_signon( $creds, false );
+    
+        if ( is_wp_error( $user ) ) {
+            echo $user->get_error_message();
+        }
+    }
+
+    return ob_get_clean();
+
+}
+add_filter('template_redirect', 'article_protector_login_form');
+// add_action( 'init', 'custom_login_form' );
+
+
+//show variation of article according to if user logged in or not
+add_filter( 'the_content', 'endContetDisplay' );
+function endContetDisplay( $content ){
+
+    if ( !is_user_logged_in() && ( isset(get_post_meta( get_the_ID(), 'paywalled')[0]) == "1" ) ) {
+        $result = '';
+        $content_substr = substr( $content, 0, 499 );
+
+        $result .=
+        '   
+            <div class="substr-content">
+                <div class="substr-overlay"></div>
+                '.$content_substr.'
+            </div>
+        ';
+
+        $result .= article_protector_login_form();
+
+        return $result;
+    }
+
+    $curr_user = wp_get_current_user();
+    $curr_username = $curr_user->user_login;
+    $get_comment = "<strong>Did you enjoy the article ".$curr_username."? Leave your thoughts below in the comments!</strong>";
+
+    return $content.$get_comment; 
+}
+
+
+/**
+ * Adding metbox to posts.
+ */
+
+//adding the article protector metabox
+add_action( 'add_meta_boxes', 'article_protector_metabox' );
+//article protector metabox field
+function article_protector_metabox(){
+    add_meta_box("article_protector_metabox", "Article protector", "article_protector_metabox_field", "post", "side");
+}
+
+//all article protector metafields
+function article_protector_metabox_field(){
+
+    //check if post premium
+    $is_premium_article = isset(get_post_meta( get_the_ID(), 'paywalled' )[0]) ? 'checked' : '';
+
+    echo '
+        <form id="articleProtectorForm" action="" method="POST">
+            <input type="checkbox" name="premium_input" id="premium_input" value="1" '.$is_premium_article.'> Premium article
         </form>
-    '.plugins_url('/public/css/article-protector.css', __FILE__);
+    ';
+    
+}
 
-    return $result;
+//saving the value of the article protector metabox field
+add_action('save_post', 'save_article_protector_meta');
+function save_article_protector_meta(){
+    update_post_meta(get_the_ID(), 'paywalled', isset($_POST["premium_input"]) ? 1 : 0 );
 }
